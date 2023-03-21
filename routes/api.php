fa6bf0c15967c1545693f8280627c83fc5b48042
Jordi Julià <jordijs6@gmail.com>
2023-03-21 11:58:53 +0100

    More cleaning up

--- routes/api.php
+++ routes/api.php
@@ -18,51 +18,36 @@ use App\Http\Controllers\UserController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-/*default preinstalled route
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
 
 //POST /players : crea un jugador/a.
-Route::post('/players', [UserAuthController::class, 'register']);
-//works     
+Route::post('/players', [UserAuthController::class, 'register']);   
 
+//Login
 Route::post('/login', [UserAuthController::class, 'login']);
-//works
 
 //GET /players/ranking: retorna el rànquing mitjà de tots els jugadors/es del sistema. És a dir, el percentatge mitjà d’èxits. //ADMIN
 Route::get('/players/ranking', [UserController::class, 'getPlayersRanking'])->middleware(['auth:api', 'role:admin']);
-//works
 
 //this one below is not required by the app, but useful for testing
 Route::get('/players/{id}', [UserController::class, 'show'])->middleware(['auth:api', 'selfplayer']);
 
-
 //PUT /players/{id} : modifica el nom del jugador/a. //THIS PLAYER
 Route::put('/players/{id}', [UserController::class, 'updateName'])->middleware('auth:api', 'selfplayer');
-//works
 
 //POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus. //THIS PLAYER
 Route::post('/players/{id}/games', [GameController::class, 'makeGame'])->middleware('auth:api', 'selfplayer');
-//works
 
 //DELETE /players/{id}/games: elimina les tirades del jugador/a. //THIS PLAYER
 Route::delete('/players/{id}/games', [GameController::class, 'deleteGamesByPlayer'])->middleware('auth:api', 'selfplayer');
-//works
 
 //GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
 Route::get('/players', [UserController::class, 'listPlayers'])->middleware(['auth:api', 'role:admin']);
-//works.
 
 //GET /players/{id}/games: retorna el llistat de jugades per un jugador/a. //THIS PLAYER
 Route::get('/players/{id}/games', [GameController::class, 'showGamesByPlayer'])->middleware(['auth:api', 'selfplayer']);
-//works.  would be better if when empty returns "you have no games"
 
 //GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit. //ADMIN
 Route::get('/players/ranking/loser', [UserController::class, 'getLoser'])->middleware(['auth:api', 'role:admin']);
-//works
 
 //GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit. //ADMIN
 Route::get('/players/ranking/winner', [UserController::class, 'getWinner'])->middleware(['auth:api', 'role:admin']);
-//works
